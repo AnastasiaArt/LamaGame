7ba57dce47e7d3fdc0888ffd21c9f2532948309e
Artemyeva Anastasiya <art-nastya@inbox.ru>
2021-05-26 23:11:35 +0300

    test mobile

--- src/js/controls.js
+++ src/js/controls.js
@@ -3,14 +3,17 @@ export class Controls {
         this.jump = false;
         this.down = false;
         this.keyMap = new Map([
-            [40, 'down'], [32, 'jump'], [13, 'enter']
+            [40,'down'],[32,'jump'],[13, 'enter']
         ]);
-        document.addEventListener('touchstart', (event) => this.update(event, true));
-        document.addEventListener('touchend', (event) => this.update(event, false));
+        document.addEventListener('keydown', (event) => this.update(event, true));
+        document.addEventListener('keyup', (event) => this.update(event, false));
     }
 
     update(event, pressed) {
-            this.jump = pressed;
-            console.log(this.jump)
+        if (this.keyMap.has(event.keyCode)) {
+            event.preventDefault();
+            event.stopPropagation();
+            this[this.keyMap.get(event.keyCode)] = pressed;
+        }
     }
 }
